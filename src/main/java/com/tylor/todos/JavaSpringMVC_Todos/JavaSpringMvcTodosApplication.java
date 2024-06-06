49450c06179d9f0c859a18693f7f5b70aed0b9d5
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-06-06 18:23:28 -0400

    Added files

---
+++ src/main/java/com/tylor/todos/JavaSpringMVC_Todos/JavaSpringMvcTodosApplication.java
@@ -0,0 +1,14 @@
+package com.tylor.todos.JavaSpringMVC_Todos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringMvcTodosApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(JavaSpringMvcTodosApplication.class, args);
+	}
+
+}
