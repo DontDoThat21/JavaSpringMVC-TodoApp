45c57d9332091ca1b7ee9d63d476593142c528e1
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-06-06 17:27:49 -0400

    Added todos html. Added TodosApp future startup/DI assistance file. Added style.css for todos table view html. Corrected references.

---
+++ src/main/java/todoapp/TodosAppApplication.java
@@ -0,0 +1,12 @@
+package main.java.todoapp;
+
+public class TodosAppApplication {
+    public static void main(String[] args) {
+        MakeNoise();
+    }
+
+    public static void MakeNoise()
+    {
+        System.out.println("Hello world from todos application startup file!");
+    }
+}
