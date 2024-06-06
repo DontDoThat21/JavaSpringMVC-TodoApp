1f30ec56f3ebbe3dc7988d767ac6bf3bf6e57b62
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-06-06 17:05:21 -0400

    Added placeholder TodosController.

---
+++ src/controllers/TodosController.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import models.Todo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class TodosController {
+
+}
